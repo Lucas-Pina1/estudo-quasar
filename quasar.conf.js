db6d9e61f3b004de697c1b2a3410d01fc77e1e11
Lucas Pina <ti.lucaspina@gmail.com>
2022-05-18 08:57:11 -0300

    validação de campos formulario, entendo select; video 13;06

--- quasar.conf.js
+++ quasar.conf.js
@@ -7,10 +7,10 @@
 // https://quasar.dev/quasar-cli/quasar-conf-js
 
 /* eslint-env node */
-const ESLintPlugin = require('eslint-webpack-plugin');
+const ESLintPlugin = require("eslint-webpack-plugin");
 /* eslint func-names: 0 */
 /* eslint global-require: 0 */
-const { configure } = require('quasar/wrappers');
+const { configure } = require("quasar/wrappers");
 
 module.exports = configure((ctx) => ({
   // https://quasar.dev/quasar-cli/supporting-ts
@@ -23,13 +23,13 @@ module.exports = configure((ctx) => ({
   // --> boot files are part of "main.js"
   // https://quasar.dev/quasar-cli/boot-files
   boot: [
-    'i18n',
-    'axios',
+    "i18n",
+    "axios",
   ],
 
   // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
   css: [
-    'app.sass',
+    "app.sass",
   ],
 
   // https://github.com/quasarframework/quasar/tree/dev/extras
@@ -42,13 +42,13 @@ module.exports = configure((ctx) => ({
     // 'line-awesome',
     // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
-    'roboto-font', // optional, you are not bound to it
-    'material-icons', // optional, you are not bound to it
+    "roboto-font", // optional, you are not bound to it
+    "material-icons", // optional, you are not bound to it
   ],
 
   // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
   build: {
-    vueRouterMode: 'hash', // available values: 'hash', 'history'
+    vueRouterMode: "hash", // available values: 'hash', 'history'
 
     // transpile: false,
     // publicPath: '/',
@@ -70,15 +70,15 @@ module.exports = configure((ctx) => ({
     // https://quasar.dev/quasar-cli/handling-webpack
     // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
     chainWebpack(chain) {
-      chain.plugin('eslint-webpack-plugin')
-        .use(ESLintPlugin, [{ extensions: ['js', 'vue'] }]);
+      chain.plugin("eslint-webpack-plugin")
+        .use(ESLintPlugin, [{ extensions: ["js", "vue"] }]);
     },
   },
 
   // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
   devServer: {
     server: {
-      type: 'http',
+      type: "http",
     },
     port: 8080,
     open: true, // opens browser window automatically
@@ -99,7 +99,9 @@ module.exports = configure((ctx) => ({
     // directives: [],
 
     // Quasar plugins
-    plugins: [],
+    plugins: [
+      "Notify",
+    ],
   },
 
   // animations: 'all', // --- includes all animations
@@ -120,61 +122,61 @@ module.exports = configure((ctx) => ({
     // Tell browser when a file from the server should expire from cache (in ms)
 
     chainWebpackWebserver(chain) {
-      chain.plugin('eslint-webpack-plugin')
-        .use(ESLintPlugin, [{ extensions: ['js'] }]);
+      chain.plugin("eslint-webpack-plugin")
+        .use(ESLintPlugin, [{ extensions: ["js"] }]);
     },
 
     middlewares: [
-      ctx.prod ? 'compression' : '',
-      'render', // keep this as last one
+      ctx.prod ? "compression" : "",
+      "render", // keep this as last one
     ],
   },
 
   // https://quasar.dev/quasar-cli/developing-pwa/configuring-pwa
   pwa: {
-    workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
+    workboxPluginMode: "GenerateSW", // 'GenerateSW' or 'InjectManifest'
     workboxOptions: {}, // only for GenerateSW
 
     // for the custom service worker ONLY (/src-pwa/custom-service-worker.[js|ts])
     // if using workbox in InjectManifest mode
     chainWebpackCustomSW(chain) {
-      chain.plugin('eslint-webpack-plugin')
-        .use(ESLintPlugin, [{ extensions: ['js'] }]);
+      chain.plugin("eslint-webpack-plugin")
+        .use(ESLintPlugin, [{ extensions: ["js"] }]);
     },
 
     manifest: {
-      name: 'Quasar App',
-      short_name: 'Quasar App',
-      description: 'A Quasar Framework app',
-      display: 'standalone',
-      orientation: 'portrait',
-      background_color: '#ffffff',
-      theme_color: '#027be3',
+      name: "Quasar App",
+      short_name: "Quasar App",
+      description: "A Quasar Framework app",
+      display: "standalone",
+      orientation: "portrait",
+      background_color: "#ffffff",
+      theme_color: "#027be3",
       icons: [
         {
-          src: 'icons/icon-128x128.png',
-          sizes: '128x128',
-          type: 'image/png',
+          src: "icons/icon-128x128.png",
+          sizes: "128x128",
+          type: "image/png",
         },
         {
-          src: 'icons/icon-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
+          src: "icons/icon-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
         },
         {
-          src: 'icons/icon-256x256.png',
-          sizes: '256x256',
-          type: 'image/png',
+          src: "icons/icon-256x256.png",
+          sizes: "256x256",
+          type: "image/png",
         },
         {
-          src: 'icons/icon-384x384.png',
-          sizes: '384x384',
-          type: 'image/png',
+          src: "icons/icon-384x384.png",
+          sizes: "384x384",
+          type: "image/png",
         },
         {
-          src: 'icons/icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
+          src: "icons/icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
         },
       ],
     },
@@ -192,7 +194,7 @@ module.exports = configure((ctx) => ({
 
   // Full list of options: https://quasar.dev/quasar-cli/developing-electron-apps/configuring-electron
   electron: {
-    bundler: 'packager', // 'packager' or 'builder'
+    bundler: "packager", // 'packager' or 'builder'
 
     packager: {
       // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
@@ -210,19 +212,19 @@ module.exports = configure((ctx) => ({
     builder: {
       // https://www.electron.build/configuration/configuration
 
-      appId: 'quasar-formularios',
+      appId: "quasar-formularios",
     },
 
     // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
     chainWebpackMain(chain) {
-      chain.plugin('eslint-webpack-plugin')
-        .use(ESLintPlugin, [{ extensions: ['js'] }]);
+      chain.plugin("eslint-webpack-plugin")
+        .use(ESLintPlugin, [{ extensions: ["js"] }]);
     },
 
     // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
     chainWebpackPreload(chain) {
-      chain.plugin('eslint-webpack-plugin')
-        .use(ESLintPlugin, [{ extensions: ['js'] }]);
+      chain.plugin("eslint-webpack-plugin")
+        .use(ESLintPlugin, [{ extensions: ["js"] }]);
     },
   },
 }));
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,19 @@
 <template>
   <q-page class="container" padding>
     <p class="text-h4">Formulário</p>
-    <div class="row q-col-gutter-md">
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="row q-col-gutter-md"
+      ref="myform"
+    >
       <q-input
         outlined
         clearable
         v-model="form.nome"
         label="Nome"
         class="col-md-12 col-sm-12 col-xs-12"
+        :rules="[(val) => (val && val.length > 0) || 'Nome obrigatório']"
       >
         <template v-slot:prepend>
           <q-icon name="person" />
@@ -20,6 +26,10 @@
         outlined
         label="Idade"
         class="col-md-12 col-sm-12 col-xs-12"
+        :rules="[
+          (val) => (val !== null && val !== '') || 'Idadde obrigatória',
+          (val) => (val > 0 && val < 100) || 'Coloque uma idade real',
+        ]"
       >
         <template v-slot:prepend>
           <q-icon name="person" />
@@ -32,21 +42,55 @@
         label="Email"
         suffix="@gmail.com"
         class="col-md-12 col-sm-12 col-xs-12"
+        :rules="[
+          (val) => (val && val.length > 0) || 'Email obrigatório',
+          (val) => validareEmail(val) || 'Coloque um email valido',
+        ]"
       >
         <template v-slot:prepend>
-          <q-icon name="mails" />
+          <q-icon name="email" />
         </template>
       </q-input>
 
       <q-input
-        v-model.number="form.telefone"
+        v-model="form.telefone"
         outlined
         label="Telefone"
         class="col-md-12 col-sm-12 col-xs-12"
+        placeholder="(xx) xxxxx-xxxx"
         mask="(##) #####-####"
         unmasked-value
+        :rules="[
+          (val) => (val && val.length > 0) || 'Telefone obrigatório',
+          (val) => val.length === 11 || 'Coloque um telefone real',
+        ]"
       />
-    </div>
+      <q-select
+        outlined
+        v-model="model"
+        :options="optionsTipoPessoa"
+        label="Tipo de pessoa"
+        emit-value
+        map-options
+        :rules="[
+          (val) => (val && val.length > 0) || 'Tipo de pessoa obrigatório',
+        ]"
+      />.
+      <div class="col-12">
+        <q-btn
+          label="Cadastrar"
+          type="submit"
+          color="primary"
+          class="float-right"
+        />
+        <q-btn
+          label="Resetar"
+          type="reset"
+          color="default"
+          class="float-right text-dark q-mr-md"
+        />
+      </div>
+    </q-form>
   </q-page>
 </template>
 
@@ -62,9 +106,39 @@ export default defineComponent({
         idade: "",
         email: "",
         telefone: "",
+        tipoPessoa: "",
       },
+      optionsTipoPesoas: [
+        { label: "Pessoa Física", value: "pf" },
+        { label: "Pessoa Jurídica", value: "pj" },
+      ],
     };
+  },
+  methods: {
+    validareEmail(email) {
+      return /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/.test(email);
+    },
+    onSubmit() {
+      this.$q.notify({
+        message: "Cadastro realizado com sucesso",
+        color: "positive",
+        icon: "check_circle_outline",
+      });
+      this.onReset();
+    },
+    async onReset() {
+      await this.resetForm();
+      this.$refs.myForm.resetValidation();
+    },
+    async resetForm() {
+      this.form = {
+        nome: "",
+        idade: "",
+        email: "",
+        telefone: "",
+        tipoPessoa: "",
+      };
+    },
   },
 });
 </script>
-s
